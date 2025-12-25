171a6aa4f714f3b3573ba10c132378e1667fd162
kalranamagar123-gif <kalranamagar123@gmail.com>
2025-12-25 15:04:13 -0500

    Day 2: Basic data types

---
+++ day02_variables/data_types.py
@@ -0,0 +1,20 @@
+## Int
+a = 7
+b = 5
+
+## Str(string)
+name = "Sita" 
+country = "USA" 
+
+#float
+y = 10.11
+x = 45.32
+
+#bool(boolean)
+is_james_student = True
+is_james_employ = False  
+
+print(type(a))
+print(type(name))
+print(type(y))
+print(type(is_james_employ))
