c2e8feedf8fb6834a6576d489426e0c4f426dd29
kalranamagar123-gif <kalranamagar123@gmail.com>
2025-12-25 15:09:23 -0500

    Add number input and basic arithmetic operations

---
+++ day02_variables/number_input.py
@@ -0,0 +1,5 @@
+randum_number = int(input("Enter a number: "))
+
+print(f"floor division by 2: {randum_number // 2}")
+print(f"Remainder when divided by 2: {randum_number % 2}")
+print(f"Square of number: {randum_number ** 2}")
