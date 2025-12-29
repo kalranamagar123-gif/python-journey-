2f606e604d7e3e7b162b09b8ef178e8dd9473838
kalranamagar123-gif <kalranamagar123@gmail.com>
2025-12-28 22:52:27 -0500

    Add functions for name, square, double, and addition

---
+++ DAY5/def_parameters_return_values.py
@@ -0,0 +1,25 @@
+def my_name(name):
+    print("my name is", name)
+my_name("kalpana")
+
+def square(n):
+    return n * n
+
+result = square(4)
+print(result)
+
+def double_num(n):
+    return n * n
+total = double_num(3)
+print(total)
+
+def add_numbers(a, b):
+    return a + b
+total = add_numbers(3, 5)
+print(total)
+
+def full_name(first, last):
+   return first + "" + last
+nam = full_name("kalpana ","rana")
+gretting = "namaste " + nam
+print(gretting)
